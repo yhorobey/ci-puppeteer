6983d28f7005c545d08a7ee2857fefd0f35c7f36
Vladislav Yatsun <vyatsun@gmail.com>
2019-08-13 13:43:49 +0400

    Initial commit

---
+++ test/index.test.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+const URL = 'http://yatsun.me/test';
+let browser, page;
+
+beforeAll(async() => {
+	browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768 });
+});
+
+describe('Compass Smart Tag', () => {
+
+	it('Test page contains a compass tag', async() => {
+	    await page.goto(URL);
+
+	    const text = await page.$eval('h1', (element) => {
+		  return element.innerHTML;
+		});
+
+	    expect(text).toBe('Welcome');
+	});
+
+});
+
+afterAll(async() => {
+	browser.close();
+});
